15816a6bd265e3a2bd42d3f795bdefa47b717dc4
movinginfo <info@movingboardsusa.com>
2025-06-30 18:04:49 +0300

    Updated app-build-manifest.json

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3b0c3139516280a8")
+/******/ 		__webpack_require__.h = () => ("6b017c468085924e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
